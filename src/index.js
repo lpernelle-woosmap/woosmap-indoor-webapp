9a36beb3e3675911e8f4d42b67d820c0608bf73c
lpernelle-wgs <79836861+lpernelle-wgs@users.noreply.github.com>
2022-12-07 16:56:29 +0100

    fix

--- src/index.js
+++ src/index.js
@@ -1,23 +1,21 @@
 import "./styles.css";
 let mapIsLoaded = null;
 
-function initMap() {
+window.initMap = function () {
   let wooskey = "woos-4fdc8f8d-e161-3fb8-9a57-e1759c71716a";
-  
+
   const queryParams = new URLSearchParams(window.location.search);
-  
+
   if (queryParams.get("key") != null) {
     wooskey = queryParams.get("key");
   }
 
-  window.woosmap.map.config.setApiKey(
-    wooskey
-  );
+  window.woosmap.map.config.setApiKey(wooskey);
 
   window.myMap = new window.woosmap.map.Map(document.getElementById("map"), {
     center: { lat: 48.844437932920535, lng: 2.3743880269761393 },
     zoom: 6,
-    gestureHandling:"greedy",
+    gestureHandling: "greedy"
   });
 
   const indoorRendererConfiguration = {
@@ -28,12 +26,15 @@ function initMap() {
   if (queryParams.get("venue") != null) {
     indoorRendererConfiguration.venue = queryParams.get("venue");
   }
-  
+
   if (queryParams.get("feature") != null) {
     indoorRendererConfiguration.highlightPOI = queryParams.get("feature");
   }
 
-  const indoorWidget = new window.woosmap.map.IndoorWidget(null, indoorRendererConfiguration);
+  const indoorWidget = new window.woosmap.map.IndoorWidget(
+    null,
+    indoorRendererConfiguration
+  );
   indoorWidget.setMap(window.myMap);
 
   let venueLoadedListener = indoorWidget.addListener(
@@ -42,8 +43,6 @@ function initMap() {
       mapIsLoaded = true;
     }
   );
+};
 
-}
-
-
-window.initMap = initMap;
+window.initMap();
